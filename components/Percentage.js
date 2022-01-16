2488c50d05816c3d11431792783165220dcbdcfd
Andrew SOUTHERN <whoamihealay@gmail.com>
2022-01-16 16:11:20 -0500

    Updates

--- components/Percentage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Percentage = ({ id, handle }) => {
-    return (<button id={id} name="percent" className="btn percent bg-dark on" onClick={() => handle(id)}>
-      {id}%
-    </button>);
-};
-exports.default = Percentage;
